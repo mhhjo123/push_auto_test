8e584da321ff9163b4d6d384f3b6cdbf262bffdc
JAVA_TEST <mskim4348>
2022-10-27 18:20:24 +0900

    18:20

--- src/jenkins/Main.java
+++ src/jenkins/Main.java
@@ -3,8 +3,8 @@ package jenkins;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		// build 및 push 작동 확인 오후 6:20
+		System.out.println("Hello World!!");
 	}
 
 }
